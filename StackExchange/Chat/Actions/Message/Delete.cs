095b3d6beed7d1867cb4650b3aa7f44763363ea0
Sam <ArcticEcho@users.noreply.github.com>
2018-03-17 18:19:12 +0000

    add delete action

---
+++ StackExchange/Chat/Actions/Message/Delete.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using RestSharp;
+
+namespace StackExchange.Chat.Actions.Message
+{
+	public class Delete : ChatAction
+	{
+		private readonly int messageId;
+
+		internal override Method RequestMethod => Method.POST;
+
+		internal override string Endpoint => $"https://{Host}/messages/{messageId}/delete";
+
+		internal override bool RequiresFKey => true;
+
+		internal override bool RequiresAuthCookies => true;
+
+		internal override ActionPermissionLevel RequiredPermissionLevel => ActionPermissionLevel.Anyone;
+
+
+
+		public Delete(int messageId)
+		{
+			this.messageId = messageId;
+		}
+
+
+
+		internal override object ProcessResponse(HttpStatusCode status, string response)
+		{
+			return status == HttpStatusCode.OK &&
+				response.ToUpperInvariant() == "\"OK\"";
+		}
+	}
+}
