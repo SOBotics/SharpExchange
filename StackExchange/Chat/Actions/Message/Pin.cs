59ccfdbdfa89d154ad56a8c80beebc4d2222a8a1
Sam <ArcticEcho@users.noreply.github.com>
2018-03-17 18:33:50 +0000

    add pin action

---
+++ StackExchange/Chat/Actions/Message/Pin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using RestSharp;
+
+namespace StackExchange.Chat.Actions.Message
+{
+	public class Pin : ChatAction
+	{
+		private readonly int messageId;
+
+		internal override Method RequestMethod => Method.POST;
+
+		internal override string Endpoint => $"https://{Host}/messages/{messageId}/owner-star";
+
+		internal override bool RequiresFKey => true;
+
+		internal override bool RequiresAuthCookies => true;
+
+		internal override ActionPermissionLevel RequiredPermissionLevel => ActionPermissionLevel.RoomOwner;
+
+
+
+		public Pin(int messageId)
+		{
+			this.messageId = messageId;
+		}
+
+
+
+		internal override object ProcessResponse(HttpStatusCode status, string response)
+		{
+			return status == HttpStatusCode.OK &&
+				response.ToUpperInvariant() == "\"OK\"";
+		}
+	}
+}
