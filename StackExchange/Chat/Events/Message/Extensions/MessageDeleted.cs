16db21e966dcfd2d6aacdb3d6c24576204c02815
Sam <ArcticEcho@users.noreply.github.com>
2018-04-01 14:53:16 +0100

    add MessageDeleted event

---
+++ StackExchange/Chat/Events/Message/Extensions/MessageDeleted.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using StackExchange.Net.WebSockets;
+
+namespace StackExchange.Chat.Events.Message.Extensions
+{
+	public static partial class Extensions
+	{
+		public static MessageDeleted AddMessageDeletedEventHandler<T>(this RoomWatcher<T> rw, Action<MessageDelete> callback) where T : IWebSocket
+		{
+			if (callback == null)
+			{
+				throw new ArgumentNullException(nameof(callback));
+			}
+
+			var eventProcessor = new MessageDeleted();
+
+			eventProcessor.OnEvent += callback;
+
+			rw.EventRouter.EventProcessors.Add(eventProcessor);
+
+			return eventProcessor;
+		}
+
+		public static MessageDeleted AddMessageDeletedEventHandler<T>(this RoomWatcher<T> rw, Action<Chat.User, int> callback) where T : IWebSocket
+		{
+			if (callback == null)
+			{
+				throw new ArgumentNullException(nameof(callback));
+			}
+
+			var eventProcessor = new MessageDeleted();
+
+			eventProcessor.OnEvent += md =>
+			{
+				var deletedBy = new Chat.User(rw.Host, md.DeletedBy);
+
+				callback(deletedBy, md.MessageId);
+			};
+
+			rw.EventRouter.EventProcessors.Add(eventProcessor);
+
+			return eventProcessor;
+		}
+	}
+}
---
+++ StackExchange/Chat/Events/Message/MessageDeleted.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace StackExchange.Chat.Events.Message
+{
+	public class MessageDelete
+	{
+		public int DeletedBy { get; internal set; }
+		public int MessageId { get; internal set; }
+	}
+
+	public class MessageDeleted : IChatEventDataProcessor, IChatEventHandler<MessageDelete>
+	{
+		public EventType Event => EventType.MessageDeleted;
+
+		public event Action<MessageDelete> OnEvent;
+
+		public void ProcessEventData(JToken data)
+		{
+			var msgId = data.Value<int>("message_id");
+			var deletedBy = data.Value<int>("user_id");
+
+			OnEvent?.Invoke(new MessageDelete
+			{
+				DeletedBy = deletedBy,
+				MessageId = msgId
+			});
+		}
+	}
+}
