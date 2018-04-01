451bccb94b902b97c7f86f4e9ca07786a3abb9d7
Sam <ArcticEcho@users.noreply.github.com>
2018-04-01 17:31:40 +0100

    add MessageReplyCreated event

---
+++ StackExchange/Chat/Events/Message/Extensions/MessageReplyCreated.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StackExchange.Net.WebSockets;
+
+namespace StackExchange.Chat.Events.Message.Extensions
+{
+	public static partial class Extensions
+	{
+		public static MessageReplyCreated AddMessageReplyCreatedEventHandler<T>(this RoomWatcher<T> rw, Action<MessageReply> callback) where T : IWebSocket
+		{
+			if (callback == null)
+			{
+				throw new ArgumentNullException(nameof(callback));
+			}
+
+			var eventProcessor = new MessageReplyCreated();
+
+			eventProcessor.OnEvent += callback;
+
+			rw.EventRouter.EventProcessors.Add(eventProcessor);
+
+			return eventProcessor;
+		}
+
+		public static MessageReplyCreated AddMessageReplyCreatedEventHandler<T>(this RoomWatcher<T> rw, Action<Chat.User, Chat.Message, Chat.Message> callback) where T : IWebSocket
+		{
+			if (callback == null)
+			{
+				throw new ArgumentNullException(nameof(callback));
+			}
+
+			var eventProcessor = new MessageReplyCreated();
+
+			eventProcessor.OnEvent += mr =>
+			{
+				var author = new Chat.User(rw.Host, mr.AuthorId);
+				var message = new Chat.Message(rw.Host, mr.MessageId, rw.AuthCookies);
+				var targetMsg = new Chat.Message(rw.Host, mr.TargetMessageId, rw.AuthCookies);
+
+				callback(author, message, targetMsg);
+			};
+
+			rw.EventRouter.EventProcessors.Add(eventProcessor);
+
+			return eventProcessor;
+		}
+	}
+}
---
+++ StackExchange/Chat/Events/Message/MessageReplyCreated.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace StackExchange.Chat.Events.Message
+{
+	public class MessageReply
+	{
+		public int AuthorId { get; internal set; }
+		public int MessageId { get; internal set; }
+		public int TargetMessageId { get; internal set; }
+	}
+
+	public class MessageReplyCreated : IChatEventDataProcessor, IChatEventHandler<MessageReply>
+	{
+		public EventType Event => EventType.MessageReply;
+
+		public event Action<MessageReply> OnEvent;
+
+		public void ProcessEventData(JToken data)
+		{
+			var msgId = data.Value<int>("message_id");
+			var targetMsgId = data.Value<int>("parent_id");
+			var msgAuthor = data.Value<int>("user_id");
+
+			OnEvent?.Invoke(new MessageReply
+			{
+				AuthorId = msgAuthor,
+				MessageId = msgId,
+				TargetMessageId = targetMsgId
+			});
+		}
+	}
+}
