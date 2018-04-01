3e22541b38d8d74fb8cd7435b4b6e39418b2399a
Sam <ArcticEcho@users.noreply.github.com>
2018-04-01 14:42:56 +0100

    add UserMentioned event

---
+++ StackExchange/Chat/Events/User/Extensions/UserMentioned.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StackExchange.Net.WebSockets;
+
+namespace StackExchange.Chat.Events.User.Extensions
+{
+	public static partial class Extensions
+	{
+		public static UserMentioned AddUserMentionedEventHandler<T>(this RoomWatcher<T> rw, Action<UserMention> callback) where T : IWebSocket
+		{
+			if (callback == null)
+			{
+				throw new ArgumentNullException(nameof(callback));
+			}
+
+			var eventProcessor = new UserMentioned();
+
+			eventProcessor.OnEvent += callback;
+
+			rw.EventRouter.EventProcessors.Add(eventProcessor);
+
+			return eventProcessor;
+		}
+
+		public static UserMentioned AddUserMentionedEventHandler<T>(this RoomWatcher<T> rw, Action<Chat.Message, Chat.User, Chat.User> callback) where T : IWebSocket
+		{
+			if (callback == null)
+			{
+				throw new ArgumentNullException(nameof(callback));
+			}
+
+			var eventProcessor = new UserMentioned();
+
+			eventProcessor.OnEvent += um =>
+			{
+				var msg = new Chat.Message(rw.Host, um.MessageId);
+				var pinger = new Chat.User(rw.Host, um.PingerId);
+				var pingee = new Chat.User(rw.Host, um.PingeeId);
+
+				callback(msg, pinger, pingee);
+			};
+
+			rw.EventRouter.EventProcessors.Add(eventProcessor);
+
+			return eventProcessor;
+		}
+	}
+}
---
+++ StackExchange/Chat/Events/User/UserMentioned.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace StackExchange.Chat.Events.User
+{
+	public class UserMention
+	{
+		public int MessageId { get; internal set; }
+		public int PingerId { get; internal set; }
+		public int PingeeId { get; internal set; }
+	}
+
+	public class UserMentioned : IChatEventDataProcessor, IChatEventHandler<UserMention>
+	{
+		public EventType Event => EventType.UserMentioned;
+
+		public event Action<UserMention> OnEvent;
+
+		public void ProcessEventData(JToken data)
+		{
+			var msgId = data.Value<int>("message_id");
+			var pingerId = data.Value<int>("user_id");
+			var pingeeId = data.Value<int>("target_user_id");
+
+			OnEvent?.Invoke(new UserMention
+			{
+				MessageId = msgId,
+				PingerId = pingerId,
+				PingeeId = pingeeId
+			});
+		}
+	}
+}
