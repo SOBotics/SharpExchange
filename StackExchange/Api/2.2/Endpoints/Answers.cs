6a4f9a24d69afc8e20dda332557954eb951cd571
Sam <ArcticEcho@users.noreply.github.com>
2018-05-17 19:20:07 +0100

    Add comments to Answers methods

--- StackExchange/Api/2.2/Endpoints/Answers.cs
+++ StackExchange/Api/2.2/Endpoints/Answers.cs
@@ -8,11 +8,17 @@ namespace StackExchange.Api.V22.Endpoints
 {
 	public static class Answers
 	{
+		/// <summary>
+		/// Gets all answers on a site.
+		/// </summary>
 		public static Result<Answer[]> GetAll(QueryOptions options = null)
 		{
 			return GetAllAsync(options).Result;
 		}
 
+		/// <summary>
+		/// Gets all answers on a site.
+		/// </summary>
 		public static async Task<Result<Answer[]>> GetAllAsync(QueryOptions options = null)
 		{
 			options = options.GetDefaultIfNull();
@@ -22,11 +28,17 @@ namespace StackExchange.Api.V22.Endpoints
 			return await ApiRequestScheduler.ScheduleRequestAsync<Answer[]>(endpoint, options);
 		}
 
+		/// <summary>
+		/// Gets answers identified by a set of ids.
+		/// </summary>
 		public static Result<Answer[]> GetByIds(IEnumerable<int> ids, QueryOptions options)
 		{
 			return GetByIdsAsync(ids, options).Result;
 		}
 
+		/// <summary>
+		/// Gets answers identified by a set of ids.
+		/// </summary>
 		public static async Task<Result<Answer[]>> GetByIdsAsync(IEnumerable<int> ids, QueryOptions options = null)
 		{
 			ids.ThrowIfNullOrEmpty(nameof(ids));
@@ -38,11 +50,17 @@ namespace StackExchange.Api.V22.Endpoints
 			return await ApiRequestScheduler.ScheduleRequestAsync<Answer[]>(endpoint, options);
 		}
 
+		/// <summary>
+		/// Gets the answers to the questions identified by a set of ids.
+		/// </summary>
 		public static Result<Answer[]> GetByQuestionIds(IEnumerable<int> questionIds, QueryOptions options)
 		{
 			return GetByQuestionIdsAsync(questionIds, options).Result;
 		}
 
+		/// <summary>
+		/// Gets the answers to the questions identified by a set of ids.
+		/// </summary>
 		public static async Task<Result<Answer[]>> GetByQuestionIdsAsync(IEnumerable<int> questionIds, QueryOptions options = null)
 		{
 			questionIds.ThrowIfNullOrEmpty(nameof(questionIds));
@@ -54,11 +72,17 @@ namespace StackExchange.Api.V22.Endpoints
 			return await ApiRequestScheduler.ScheduleRequestAsync<Answer[]>(endpoint, options);
 		}
 
+		/// <summary>
+		/// Gets the answers posted by the users identified by a set of ids.
+		/// </summary>
 		public static Result<Answer[]> GetByUserIds(IEnumerable<int> userIds, QueryOptions options)
 		{
 			return GetByUserIdsAsync(userIds, options).Result;
 		}
 
+		/// <summary>
+		/// Gets the answers posted by the users identified by a set of ids.
+		/// </summary>
 		public static async Task<Result<Answer[]>> GetByUserIdsAsync(IEnumerable<int> userIds, QueryOptions options = null)
 		{
 			userIds.ThrowIfNullOrEmpty(nameof(userIds));
@@ -70,11 +94,17 @@ namespace StackExchange.Api.V22.Endpoints
 			return await ApiRequestScheduler.ScheduleRequestAsync<Answer[]>(endpoint, options);
 		}
 
+		/// <summary>
+		/// Gets the top answers a user has posted on questions with a set of tags.
+		/// </summary>
 		public static Result<Answer[]> GetTopAnswersByUserByTags(int userId, IEnumerable<string> tags, QueryOptions options = null)
 		{
 			return GetTopAnswersByUserByTagsAsync(userId, tags, options).Result;
 		}
 
+		/// <summary>
+		/// Gets the top answers a user has posted on questions with a set of tags.
+		/// </summary>
 		public static async Task<Result<Answer[]>> GetTopAnswersByUserByTagsAsync(int userId, IEnumerable<string> tags, QueryOptions options = null)
 		{
 			tags.ThrowIfNullOrEmpty(nameof(tags));
