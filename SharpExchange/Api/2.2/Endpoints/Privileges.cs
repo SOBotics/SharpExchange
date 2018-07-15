581c2ed482dd1972eb6358dbcb056668eb692ff8
Sam <ArcticEcho@users.noreply.github.com>
2018-07-15 13:55:03 +0100

    Add privilege api endpoints

---
+++ SharpExchange/Api/2.2/Endpoints/Privileges.cs
@@ -0,0 +1,35 @@
+﻿using SharpExchange.Api.V22.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpExchange.Api.V22.Endpoints
+{
+	public static class Privileges
+	{
+		/// <summary>
+		/// Gets all the privileges available on a site.
+		/// </summary>
+		public static Task<Result<Privilege[]>> GetAllAsync(QueryOptions options = null)
+		{
+			options = options.GetDefaultIfNull();
+
+			var endpoint = $"{Constants.BaseApiUrl}/privileges";
+
+			return ApiRequestScheduler.ScheduleRequestAsync<Privilege[]>(endpoint, options);
+		}
+
+		/// <summary>
+		/// Gets the privileges the given user has on a site.
+		/// </summary>
+		public static Task<Result<Privilege[]>> GetByUserIdAsync(int userId, QueryOptions options = null)
+		{
+			options = options.GetDefaultIfNull();
+
+			var endpoint = $"{Constants.BaseApiUrl}/users/{userId}/privileges";
+
+			return ApiRequestScheduler.ScheduleRequestAsync<Privilege[]>(endpoint, options);
+		}
+	}
+}
---
+++ SharpExchange/Api/2.2/Types/Privilege.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace SharpExchange.Api.V22.Types
+{
+	public class Privilege
+	{
+		[JsonProperty("description")]
+		public string Description { get; internal set; }
+
+		[JsonProperty("short_description")]
+		public string ShortDescription { get; internal set; }
+
+		[JsonProperty("reputation")]
+		public int RequiredReputation { get; internal set; }
+	}
+}
