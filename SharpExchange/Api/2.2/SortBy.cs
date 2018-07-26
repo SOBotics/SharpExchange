aee7651c194f9d8f4bf0f43e562afe9e40ec753f
Sam <ArcticEcho@users.noreply.github.com>
2018-07-26 18:41:43 +0100

    nice

--- SharpExchange/Api/2.2/SortBy.cs
+++ SharpExchange/Api/2.2/SortBy.cs
@@ -2,34 +2,41 @@
 {
 	public enum SortBy
 	{
-		[ApiQueryValue("creation_date")]
-		CreationDate,
+		//TODO: We done fucked up.
+		//[ApiQueryValue("creation")]
+		//CreationDate,
 
-		[ApiQueryValue("votes")]
-		Votes,
+		//[ApiQueryValue("votes")]
+		//Votes,
 
-		[ApiQueryValue("last_activity_date")]
-		PostAcivity,
+		//[ApiQueryValue("last_activity_date")]
+		//PostAcivity,
 
-		[ApiQueryValue("rank")]
-		BadgeRank,
+		//[ApiQueryValue("rank")]
+		//BadgeRank,
 
-		[ApiQueryValue("name")]
-		BadgeName,
+		//[ApiQueryValue("name")]
+		//BadgeName,
 
-		[ApiQueryValue("type")]
-		BadgeType,
+		//[ApiQueryValue("type")]
+		//BadgeType,
 
-		[ApiQueryValue("approval_date")]
-		EditApprovalDate,
+		//[ApiQueryValue("approval_date")]
+		//EditApprovalDate,
 
-		[ApiQueryValue("rejection_date")]
-		EditRejectionDate,
+		//[ApiQueryValue("rejection_date")]
+		//EditRejectionDate,
 
-		[ApiQueryValue("count")]
-		TagPopularity,
+		//[ApiQueryValue("count")]
+		//TagPopularity,
 
-		[ApiQueryValue("name")]
-		TagName,
+		//[ApiQueryValue("name")]
+		//TagName,
+
+		//[ApiQueryValue("applied_count")]
+		//TagSynonymAppliedCount,
+
+		//[ApiQueryValue("last_applied_date")]
+		//TagSynonymActivity,
 	}
 }
