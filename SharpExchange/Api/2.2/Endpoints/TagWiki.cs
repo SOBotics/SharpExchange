f4b5dc69427655b88f66e43efcaf2e4be749a2bc
Sam <ArcticEcho@users.noreply.github.com>
2018-07-26 19:51:37 +0100

    add tag wiki api support

---
+++ SharpExchange/Api/2.2/Endpoints/TagWiki.cs
@@ -0,0 +1,27 @@
+﻿using SharpExchange.Api.V22.Types;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SharpExchange.Api.V22.Endpoints
+{
+	public static class TagWikis
+	{
+		/// <summary>
+		/// Gets the wiki entries for a set of tags.
+		/// </summary>
+		public static Task<Result<TagWiki[]>> GetByTagNamesAsync(IEnumerable<string> tagNames, QueryOptions options = null)
+		{
+			tagNames.ThrowIfNullOrEmpty(nameof(tagNames));
+			options = options.GetDefaultIfNull();
+
+			var tagList = tagNames
+				.Select(WebUtility.UrlEncode)
+				.ToDelimitedList();
+			var endpoint = $"{Constants.BaseApiUrl}/tags/{tagList}/wikis";
+
+			return ApiRequestScheduler.ScheduleRequestAsync<TagWiki[]>(endpoint, options);
+		}
+	}
+}
---
+++ SharpExchange/Api/2.2/Types/TagWiki.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+
+namespace SharpExchange.Api.V22.Types
+{
+	public class TagWiki
+	{
+		[JsonProperty("body")]
+		public string Body { get; internal set; }
+
+		[JsonProperty("body_last_edit_date")]
+		[JsonConverter(typeof(UnixDateTimeConverter))]
+		public DateTime? BodyLastEditedOn { get; internal set; }
+
+		[JsonProperty("excerpt")]
+		public string Excerpt { get; internal set; }
+
+		[JsonProperty("excerpt_last_edit_date")]
+		[JsonConverter(typeof(UnixDateTimeConverter))]
+		public DateTime? ExcerptLastEditedOn { get; internal set; }
+
+		[JsonProperty("last_body_editor")]
+		public ShallowUser LastBodyEditor { get; internal set; }
+
+		[JsonProperty("last_excerpt_editor")]
+		public ShallowUser LastExceprtEditor { get; internal set; }
+
+		[JsonProperty("tag_name")]
+		public string TagName { get; internal set; }
+	}
+}
