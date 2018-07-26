6547acdca4c4b5aeb3864189ff5b3a05daf4ecf4
Sam <ArcticEcho@users.noreply.github.com>
2018-07-26 18:42:04 +0100

    add tag synonym api endpoint/type #2

---
+++ SharpExchange/Api/2.2/Endpoints/TagSynonyms.cs
@@ -0,0 +1,41 @@
+﻿using SharpExchange.Api.V22.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpExchange.Api.V22.Endpoints
+{
+	public static class TagSynonyms
+	{
+		/// <summary>
+		/// Gets all the tag synonyms on a site.
+		/// </summary>
+		public static Task<Result<TagSynonym[]>> GetAllAsync(QueryOptions options = null)
+		{
+			options = options.GetDefaultIfNull();
+
+			var endpoint = $"{Constants.BaseApiUrl}/tags/synonyms";
+
+			return ApiRequestScheduler.ScheduleRequestAsync<TagSynonym[]>(endpoint, options);
+		}
+
+		/// <summary>
+		/// Gets the synonyms for a specific set of tags.
+		/// </summary>
+		public static Task<Result<TagSynonym[]>> GetByIdsAsync(IEnumerable<string> tags, QueryOptions options = null)
+		{
+			tags.ThrowIfNullOrEmpty(nameof(tags));
+			options = options.GetDefaultIfNull();
+
+			var tagList = tags
+				.Select(WebUtility.UrlEncode)
+				.ToDelimitedList();
+			var endpoint = $"{Constants.BaseApiUrl}/tags/{tagList}/synonyms";
+
+			return ApiRequestScheduler.ScheduleRequestAsync<TagSynonym[]>(endpoint, options);
+		}
+	}
+}
---
+++ SharpExchange/Api/2.2/Types/TagSynonym.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+
+namespace SharpExchange.Api.V22.Types
+{
+	public class TagSynonym
+	{
+		[JsonProperty("applied_count")]
+		public int? AppliedCount { get; internal set; }
+
+		[JsonProperty("creation_date")]
+		[JsonConverter(typeof(UnixDateTimeConverter))]
+		public DateTime? CreatedOn { get; internal set; }
+
+		[JsonProperty("from_tag")]
+		public string FromTag { get; internal set; }
+
+		[JsonProperty("last_applied_date")]
+		[JsonConverter(typeof(UnixDateTimeConverter))]
+		public DateTime? LastAppliedOn { get; internal set; }
+
+		[JsonProperty("to_tag")]
+		public string ToTag { get; internal set; }
+	}
+}
