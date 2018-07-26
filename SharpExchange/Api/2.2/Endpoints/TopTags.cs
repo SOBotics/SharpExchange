efbabe678b3e3b96aecd0ae894834cc746c1aa95
Sam <ArcticEcho@users.noreply.github.com>
2018-07-26 20:55:38 +0100

    add top tags #2

---
+++ SharpExchange/Api/2.2/Endpoints/TopTags.cs
@@ -0,0 +1,44 @@
+﻿using SharpExchange.Api.V22.Types;
+using System.Threading.Tasks;
+
+namespace SharpExchange.Api.V22.Endpoints
+{
+	public static class TopTags
+	{
+		/// <summary>
+		/// Gets the top tags (by score) a single user has posted answers in.
+		/// </summary>
+		public static Task<Result<TopTag[]>> GetByAnswersAsync(int userId, QueryOptions options = null)
+		{
+			options = options.GetDefaultIfNull();
+
+			var endpoint = $"{Constants.BaseApiUrl}/users/{userId}/top-answer-tags";
+
+			return ApiRequestScheduler.ScheduleRequestAsync<TopTag[]>(endpoint, options);
+		}
+
+		/// <summary>
+		/// Gets the top tags (by score) a single user has asked questions in.
+		/// </summary>
+		public static Task<Result<TopTag[]>> GetByQuestionsAsync(int userId, QueryOptions options = null)
+		{
+			options = options.GetDefaultIfNull();
+
+			var endpoint = $"{Constants.BaseApiUrl}/users/{userId}/top-question-tags";
+
+			return ApiRequestScheduler.ScheduleRequestAsync<TopTag[]>(endpoint, options);
+		}
+
+		/// <summary>
+		/// Gets the top tags (by score) a single user has posted in.
+		/// </summary>
+		public static Task<Result<TopTag[]>> GetAllAsync(int userId, QueryOptions options = null)
+		{
+			options = options.GetDefaultIfNull();
+
+			var endpoint = $"{Constants.BaseApiUrl}/users/{userId}/top-tags";
+
+			return ApiRequestScheduler.ScheduleRequestAsync<TopTag[]>(endpoint, options);
+		}
+	}
+}
---
+++ SharpExchange/Api/2.2/Types/TopTag.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace SharpExchange.Api.V22.Types
+{
+	public class TopTag
+	{
+		[JsonProperty("answer_count")]
+		public int? AnswerCount { get; internal set; }
+
+		[JsonProperty("answer_score")]
+		public int? AnswerScore { get; internal set; }
+
+		[JsonProperty("question_count")]
+		public int? QuestionCount { get; internal set; }
+
+		[JsonProperty("question_score")]
+		public int? QuestionScore { get; internal set; }
+
+		[JsonProperty("tag_name")]
+		public string TagName { get; internal set; }
+
+		[JsonProperty("user_id")]
+		public int? UserId { get; internal set; }
+	}
+}
