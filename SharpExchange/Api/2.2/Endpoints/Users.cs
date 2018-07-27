f90dc9217d38b80625d93060e25b92801826d181
Sam <ArcticEcho@users.noreply.github.com>
2018-07-27 11:37:44 +0100

    add users #2

---
+++ SharpExchange/Api/2.2/Endpoints/Users.cs
@@ -0,0 +1,61 @@
+﻿using SharpExchange.Api.V22.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpExchange.Api.V22.Endpoints
+{
+	public static class Users
+	{
+		/// <summary>
+		/// Gets all the users on a site.
+		/// </summary>
+		public static Task<Result<User[]>> GetAllAsync(QueryOptions options = null)
+		{
+			options = options.GetDefaultIfNull();
+
+			var endpoint = $"{Constants.BaseApiUrl}/users";
+
+			return ApiRequestScheduler.ScheduleRequestAsync<User[]>(endpoint, options);
+		}
+
+		/// <summary>
+		/// Gets the users identified by a set of ids.
+		/// </summary>
+		public static Task<Result<User[]>> GetByIdsAsync(IEnumerable<int> userIds, QueryOptions options = null)
+		{
+			userIds.ThrowIfNullOrEmpty(nameof(userIds));
+			options = options.GetDefaultIfNull();
+
+			var idsStr = userIds.ToDelimitedList();
+			var endpoint = $"{Constants.BaseApiUrl}/users/{idsStr}";
+
+			return ApiRequestScheduler.ScheduleRequestAsync<User[]>(endpoint, options);
+		}
+
+		/// <summary>
+		/// Gets the users who have moderation powers on the site.
+		/// </summary>
+		public static Task<Result<User[]>> GetModeratorsAsync(QueryOptions options = null)
+		{
+			options = options.GetDefaultIfNull();
+
+			var endpoint = $"{Constants.BaseApiUrl}/users/moderators";
+
+			return ApiRequestScheduler.ScheduleRequestAsync<User[]>(endpoint, options);
+		}
+
+		/// <summary>
+		/// Gets the users who are active moderators who have also won a moderator election.
+		/// </summary>
+		public static Task<Result<User[]>> GetElectedModeratorsAsync(QueryOptions options = null)
+		{
+			options = options.GetDefaultIfNull();
+
+			var endpoint = $"{Constants.BaseApiUrl}/users/moderators/elected";
+
+			return ApiRequestScheduler.ScheduleRequestAsync<User[]>(endpoint, options);
+		}
+	}
+}
---
+++ SharpExchange/Api/2.2/Types/User.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+
+namespace SharpExchange.Api.V22.Types
+{
+	public class User : ShallowUser
+	{
+		[JsonProperty("about_me")]
+		public string AboutMe { get; internal set; }
+
+		[JsonProperty("answer_count")]
+		public int? AnswerCount { get; internal set; }
+
+		[JsonProperty("creation_date")]
+		[JsonConverter(typeof(UnixDateTimeConverter))]
+		public DateTime? CreatedOn { get; internal set; }
+
+		[JsonProperty("down_vote_count")]
+		public int? DownvoteCount { get; internal set; }
+
+		[JsonProperty("is_employee")]
+		public bool? IsEmployee { get; internal set; }
+
+		[JsonProperty("last_access_date")]
+		[JsonConverter(typeof(UnixDateTimeConverter))]
+		public DateTime? LastAccessedOn { get; internal set; }
+
+		[JsonProperty("last_modified_date")]
+		[JsonConverter(typeof(UnixDateTimeConverter))]
+		public DateTime? LastModifiedOn { get; internal set; }
+
+		[JsonProperty("location")]
+		public string Location { get; internal set; }
+
+		[JsonProperty("question_count")]
+		public int? QuestionCount { get; internal set; }
+
+		[JsonProperty("reputation_change_day")]
+		public int? ReputationChangeDay { get; internal set; }
+
+		[JsonProperty("reputation_change_month")]
+		public int? ReputationChangeMonth { get; internal set; }
+
+		[JsonProperty("reputation_change_quarter")]
+		public int? ReputationChangeQuarter { get; internal set; }
+
+		[JsonProperty("reputation_change_week")]
+		public int? ReputationChangeWeek { get; internal set; }
+
+		[JsonProperty("reputation_change_year")]
+		public int? ReputationChangeYear { get; internal set; }
+
+		[JsonProperty("timed_penalty_date")]
+		[JsonConverter(typeof(UnixDateTimeConverter))]
+		public DateTime? SuspensionEndsOn { get; internal set; }
+
+		[JsonProperty("up_vote_count")]
+		public int? UpvoteCount { get; internal set; }
+
+		[JsonProperty("view_count")]
+		public int? ViewCount { get; internal set; }
+
+		[JsonProperty("website_url")]
+		public string WebsiteUrl { get; internal set; }
+	}
+}
